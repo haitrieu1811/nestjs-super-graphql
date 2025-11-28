b2037e6a78f893fa414ad82785f8e5d51e7cc96e
Tran Hai Trieu <haitrieu2524@gmail.com>
2025-11-28 21:20:22 +0700

    Mutation Graphql

--- src/modules/user.resolver.ts
+++ src/modules/user.resolver.ts
@@ -1,6 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
 
-import { User } from 'src/modules/user.dto'
+import { CreateUserInput, UpdateUserInput, User } from 'src/modules/user.dto'
 import { UserService } from 'src/modules/user.service'
 
 @Resolver()
@@ -10,5 +10,28 @@ export class UserResolver {
   @Query(() => [User], { name: 'users' })
   findAll() {
     return this.userService.findAll()
+  }
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.userService.findOne(id)
+  }
+
+  @Mutation(() => User, { name: 'createUser' })
+  create(@Args('createUserInput') createUserInput: CreateUserInput): Promise<User> {
+    return this.userService.create(createUserInput)
+  }
+
+  @Mutation(() => User, { name: 'updateUser' })
+  update(
+    @Args('userId', { type: () => Int }) userId: number,
+    @Args('updateUserInput') updateUserInput: UpdateUserInput,
+  ): Promise<User> {
+    return this.userService.update({ userId, updateUserInput })
+  }
+
+  @Mutation(() => User, { name: 'deleteUser' })
+  delete(@Args('userId', { type: () => Int }) userId: number): Promise<User> {
+    return this.userService.delete(userId)
   }
 }
--- src/modules/user.service.ts
+++ src/modules/user.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
+
+import { CreateUserInput, UpdateUserInput, User } from 'src/modules/user.dto'
 
 @Injectable()
 export class UserService {
@@ -19,7 +21,56 @@ export class UserService {
     },
   ]
 
-  findAll() {
+  findAll(): Promise<User[]> {
     return Promise.resolve(this.users)
+  }
+
+  findOne(id: number): Promise<User> {
+    const user = this.users.find((user) => user.id === id)
+    if (!user) {
+      throw new NotFoundException('UserNotFound')
+    }
+    return Promise.resolve(user)
+  }
+
+  create(createUserInput: CreateUserInput): Promise<User> {
+    const newUser: User = {
+      id: this.users[this.users.length - 1].id++,
+      name: createUserInput.name,
+      email: createUserInput.email,
+      password: createUserInput.password,
+      createdAt: new Date().toISOString(),
+    }
+    this.users.push(newUser)
+    return Promise.resolve(newUser)
+  }
+
+  update({ updateUserInput, userId }: { updateUserInput: UpdateUserInput; userId: number }): Promise<User> {
+    const index = this.users.findIndex((user) => user.id === userId)
+    if (index === -1) {
+      throw new NotFoundException('UserNotFound')
+    }
+    this.users = this.users.map((user) => {
+      if (user.id === userId) {
+        return {
+          ...user,
+          name: updateUserInput.name ?? user.name,
+          email: updateUserInput.email ?? user.email,
+          password: updateUserInput.password ?? user.password,
+        }
+      }
+      return user
+    })
+    return Promise.resolve(this.users.find((user) => user.id === userId) as User)
+  }
+
+  delete(userId: number): Promise<User> {
+    const index = this.users.findIndex((user) => user.id === userId)
+    if (index === -1) {
+      throw new NotFoundException('UserNotFound')
+    }
+    const user = this.users.find((user) => user.id === userId) as User
+    this.users.splice(index, 1)
+    return Promise.resolve(user)
   }
 }
